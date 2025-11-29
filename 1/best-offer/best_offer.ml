2f4355af8734d8cc60570a0a029876a152f332a5
Ghirons <fghironi59@gmail.com>
2025-11-29 13:18:19 +0100

    modifiche lv1

--- 1/best-offer/best_offer.ml
+++ 1/best-offer/best_offer.ml
@@ -8,3 +8,15 @@ let max (a:int option) (b:int option) :int option =
 let best_offer (a:int option) (b:int option) (c:int option): int option = 
   if (a=None && b=None && c=None) then None
   else max (max a b) c;;
+
+
+let best_offer (a:int option) (b:int option) (c:int option): int option = 
+if (a=None && b=None && c=None) then None
+else let max (d:int option) (e:int option) :int option = 
+  match (d, e) with
+  |None, None -> None
+  |None, _ -> e
+  |_, None -> d
+  |_, _ when d>=e -> d
+  |_, _ -> e in
+  max (max a b) c;;
--- 1/movie-ratings/movie_rating.ml
+++ 1/movie-ratings/movie_rating.ml
@@ -1,9 +1,9 @@
-let movie_rating a b c :string = 
+let movie_rating_2 a b c :string = 
   if a+b+c>15 then failwith "impossibile"
   else if a<1 || b<1 || c<1 then failwith "impossibile"
-  else begin
-    if a+b+c=15 then "Masterpiece"
-    else if a+b+c>=14 && ((a>=5 && b>=5)||(a>=5 && c>=5)||(c>=5 && b>=5)) then "Highly Recommended"
-    else if a+b+c>=11 && ((a>=4 && b>=4)||(a>=4 && c>=4)||(c>=4 && b>=4)) then "Recommended"
-    else "Mixed Reviews"
-  end;;
+  else if a>5 || b>5 || c>5 then failwith "impossibile"
+  else match (a+b+c) with
+  |15 -> "Masterpiece" 
+  |14 when (a=5 && b=5 || a=5 && c=5 || b=5 && c=5) -> "Highly"
+  |d when (d>=11 && (a=4 && b=4 && c>=3) || (a=4 && c=4 && b>=3) || (c=4 && b=4 && a>=3)) -> "Raccomended"
+  |_ -> "Mixed";;
